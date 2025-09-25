1533ab58e02126b3bfd86898e547b9ef14401aec
TheSwimon <lordkipanidzegeorge@gmail.com>
2025-09-26 01:16:11 +0400

    Removed Unnecessary code

--- Program.cs
+++ Program.cs
@@ -4,13 +4,6 @@ using System.Reflection.Metadata;
 using System.Text.Json;
 
 
-
-
-//JsonDataHandler handler = new JsonDataHandler();
-//List<Expense> expenses = handler.ExpenseContainer.Expenses;
-//ExpenseManager manager = new ExpenseManager();
-
-
 CommandHandler commandHandler = new CommandHandler();
 commandHandler.ExecuteCommand(args);
 
